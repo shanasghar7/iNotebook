e27b6c732fa778c8e89d3904404c11e81463977d
Shan <shanasghar7@gmail.com>
2022-02-24 17:12:44 +0500

    updated repository

---
+++ Backend/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+
+const JWT_SECRET = 'Harryisagoodb$oy';
